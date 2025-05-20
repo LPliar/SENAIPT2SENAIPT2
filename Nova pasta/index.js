b947065b3e6206f8150ea37c3218da6eeb2a1be3
LPliar <gabrielstoffell01@gmail.com>
2025-05-20 16:57:38 -0300

    JS JSJSJ JSJ JSJSJJSJSJSJJSJ
    É JS NUM SEI MAS ERAP RA SER

---
+++ Nova pasta/index.js
@@ -0,0 +1,9 @@
+
+// COMENTARIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+nomeFuncao = function(){
+    altura = prompt("Digite a altura: ")
+    largura =  prompt("Digite o comprimetooo")
+     altura * largura
+     document.write("<p>METRO QUADRADO<\p>") + altura * largura
+     
+}
