3c1baef43677b7295f41b9466984a1676e5f4264
LPliar <gabrielstoffell01@gmail.com>
2025-06-12 17:09:53 -0300

    CHEGAAAAAAAAAAAAAAAA (SO EXPLICAÇAO DE PTRHON)

---
+++ Nome da pasta é/respostaempython.py
@@ -0,0 +1,45 @@
+def quizNUMER1 ():
+ quiz = 1
+ quiz2 = 2
+ quiz3 = 3 
+ quiz4 = 4
+ quiz5 = 5
+ if quiz == 1:
+  print("Energia cinetica, energia mecanica, energia termica ")
+ elif quiz2 == 2:
+    print("Correndo, andando, carregar celular ")
+ elif quiz3 == 3:
+    print("10000g")
+ elif quiz4 == 4:
+    print("0,008")
+ elif quiz5 == 5:
+    print("5000")
+def two ():
+    quiz = 1 
+    quiz2 = 2
+    quiz3 = 3 
+    quiz4 = 4
+    quiz5 = 5
+    if quiz == 1:
+     print(" Uma energia em funcionamento  ")
+    elif quiz2 == 2:
+     print("Componentes do atomo eletron negativo proton positivo e neutrons é a carga neutra ")
+    elif quiz3 == 3:
+     print(" cargas opostas se atraem e diferentes ")
+    elif quiz4 == 4:
+     print("negativa")
+    elif quiz5 == 5:
+     print("D posotibva")
+# a) 1,2 a = 1200 ma
+# b)15UA(estranho) = 0,0015mA
+# c) 350mA = 0,350A
+#1)  a) V b)V c)F
+#2)Negativo
+#3)Sim, pq 1 pode estar mais negativo que o outro 
+#4) vats = potencia, tensao = wolts
+#5) ---- ???
+#6) COntinuo ele nao muda a frequencia, alternada ele altaerna rapidamente em picos  iguais ex: 10 max -10 max
+#7) *Desenho da frequencia continua e alternada
+#8) Que ele precisa receber 1000W de potencia para funcionar exempo: vai consumir mais um chuveiro de 5000 para um de 2500
+#9) p = V.I = p = 3800 . 4 = p 15200W, p = 1,52kw
+#10) A) 60hz
